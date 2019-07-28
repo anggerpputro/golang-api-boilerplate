4617730beee67ce8b92945367baeb97156bb4f6f
Angger P. Putro @(HP :-- Ubuntu) <anggerpputro@gmail.com>
2019-07-28 16:35:59 +0700

    handle request using DefaultServeMux

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ import (
 	"net/url"
 )
 
-type ctallHandler int
-
-func (ch ctallHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func dog(w http.ResponseWriter, req *http.Request) {
 	err := req.ParseForm()
 	if err != nil {
 		panic(err)
@@ -30,6 +28,38 @@ func (ch ctallHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		req.ContentLength,
 	}
 
+	fmt.Fprintln(w, "DOG DOG DOG")
+	fmt.Fprintf(w, "Method: %s \n", responseData.Method)
+	fmt.Fprintf(w, "URL: %s \n", responseData.URL)
+	fmt.Fprintf(w, "Submissions: %s \n", responseData.Submissions)
+	fmt.Fprintf(w, "Header: %s \n", responseData.Header)
+	fmt.Fprintf(w, "Host: %s \n", responseData.Host)
+	fmt.Fprintf(w, "ContentLength: %d \n", responseData.ContentLength)
+}
+
+func cat(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	responseData := struct {
+		Method        string
+		URL           *url.URL
+		Submissions   map[string][]string
+		Header        http.Header
+		Host          string
+		ContentLength int64
+	}{
+		req.Method,
+		req.URL,
+		req.Form,
+		req.Header,
+		req.Host,
+		req.ContentLength,
+	}
+
+	fmt.Fprintln(w, "CAT CAT CAT")
 	fmt.Fprintf(w, "Method: %s \n", responseData.Method)
 	fmt.Fprintf(w, "URL: %s \n", responseData.URL)
 	fmt.Fprintf(w, "Submissions: %s \n", responseData.Submissions)
@@ -39,6 +69,8 @@ func (ch ctallHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	var ch ctallHandler
-	http.ListenAndServe(":8080", ch)
+	http.HandleFunc("/dog", dog)
+	http.HandleFunc("/cat/", cat)
+
+	http.ListenAndServe(":8080", nil)
 }
