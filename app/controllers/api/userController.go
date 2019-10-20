2bd8da4165e9789169a28309cdee5b5d5c047fb1
Angger P. Putro @(HP :-- Ubuntu) <anggerpputro@gmail.com>
2019-10-20 09:30:05 +0700

    finish crud users

--- app/controllers/api/userController.go
+++ app/controllers/api/userController.go
@@ -96,22 +96,123 @@ func (c UserController) Store(gc *gin.Context) {
 	}
 
 	core.NewJsonResponse().ResponseSuccess(gc, gin.H{
-		"store": user,
+		"stored": user,
 	})
 }
 
 func (c UserController) Update(gc *gin.Context) {
 	id := gc.Param("id")
 
+	db := ctlDB.GetDefaultConnection()
+	defer db.Close()
+
+	var user ctlModel.User
+	// Get first matched record
+	db.First(&user, id)
+
+	if user.ID == 0 {
+		errMessage := fmt.Sprintf("User with ID: %v not found!", id)
+		core.NewJsonResponse().ResponseNotFound(gc, gin.H{
+			"error": map[string]string{
+				"message": errMessage,
+			},
+		})
+		log.Panicln(errMessage)
+	}
+
+	// Note the use of tx as the database handle once you are within a transaction
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		core.NewJsonResponse().ResponseError(gc, gin.H{
+			"error": err,
+		})
+		log.Panicln(err)
+	}
+
+	user.Name = gc.PostForm("name")
+	user.Username = gc.PostForm("username")
+	user.Email = gc.PostForm("email")
+	user.Password = gc.PostForm("password")
+
+	if err := tx.Save(&user).Error; err != nil {
+		tx.Rollback()
+		core.NewJsonResponse().ResponseError(gc, gin.H{
+			"error": err,
+		})
+		log.Panicln(err)
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		core.NewJsonResponse().ResponseError(gc, gin.H{
+			"error": err,
+		})
+		log.Panicln(err)
+	}
+
 	core.NewJsonResponse().ResponseSuccess(gc, gin.H{
-		"update": id,
+		"updated": user,
 	})
 }
 
 func (c UserController) Destroy(gc *gin.Context) {
 	id := gc.Param("id")
 
+	db := ctlDB.GetDefaultConnection()
+	defer db.Close()
+
+	var user ctlModel.User
+	// Get first matched record
+	db.First(&user, id)
+
+	if user.ID == 0 {
+		errMessage := fmt.Sprintf("User with ID: %v not found!", id)
+		core.NewJsonResponse().ResponseNotFound(gc, gin.H{
+			"error": map[string]string{
+				"message": errMessage,
+			},
+		})
+		log.Panicln(errMessage)
+	}
+
+	// Note the use of tx as the database handle once you are within a transaction
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		core.NewJsonResponse().ResponseError(gc, gin.H{
+			"error": err,
+		})
+		log.Panicln(err)
+	}
+
+	if err := tx.Delete(&user).Error; err != nil {
+		tx.Rollback()
+		core.NewJsonResponse().ResponseError(gc, gin.H{
+			"error": err,
+		})
+		log.Panicln(err)
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		core.NewJsonResponse().ResponseError(gc, gin.H{
+			"error": err,
+		})
+		log.Panicln(err)
+	}
+
 	core.NewJsonResponse().ResponseSuccess(gc, gin.H{
-		"destroy": id,
+		"destroyed": id,
 	})
 }
